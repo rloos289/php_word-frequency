e8c9de26d60dbce00adabf9dff76bd6210d96fbe
Ryan Loos <RLoos289@gmail.com>
2016-09-16 12:54:03 -0700

    set up basic page requirements and finished README

---
+++ README.md
@@ -0,0 +1,51 @@
+# _Word Frequency_
+
+#### _Counts how many times a word is in a sentence, 9/16_
+
+#### By _**Ryan Loos**_
+
+## Setup/Installation Requirements
+
+* _Clone this repository to your desktop_
+* _Run composer install in Terminal_
+* _start a server in web directory (php -S localhost:8000)_
+
+_You must host this webpage locally_
+
+## Behavior Driven Development
+* _take a word and strip it of any surrounding punctuation and whitespace, and make it lowercase_
+  * _input: Inconceivable!_
+  * _output: inconceivable_
+
+* _break a sentence into individual words and run the previous spec on each word_
+  * _input: Defeat me? That's inconceivable!_
+  * _output: _[defeat, me, that's, inconceivable]_
+
+* _check if the word appears in the sentence_
+  * _input: Sentence=[defeat, me, that's, inconceivable] Word = inconceivable_
+  * _output: true_
+* _print out the number of times the word appears in the sentence_
+  * _input: Sentence=[defeat, me, that's, inconceivable, inconceivable, i, say] Word = inconceivable_
+  * _output: 2
+
+
+## Known Bugs
+
+_None yet_
+
+## Support and contact details
+
+_Ryan Loos: Rloos289@gmail.com_
+
+## Technologies Used
+
+_HTML,
+PHP,
+TWIG 1.0,
+SILEX 1.1_
+
+### License
+
+*This webpage is licensed under the GPL license.*
+
+Copyright (c) 2016 **_Ryan Loos_**
---
+++ tests/TemplateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+
+    //run test in terminal: ./vendor/bin/phpunit tests
+
+    //on Mac: run: export PATH=$PATH:./vendor/bin
+    //then run phpunit tests
+    {
+
+    }
+      // Testcode example
+      //  function test_makeTitleCase_oneWord()
+      //  {
+      //      //Arrange
+      //      $test_TitleCaseGenerator = new Template;
+      //      $input = "beowulf";
+       //
+      //      //Act
+      //      $result = $test_TitleCaseGenerator->testTemplate($input);
+       //
+      //      //Assert
+      //      $this->assertEquals("Beowulf", $result);
+      //  }
+
+        //TEST FOR LOOPING THROUGH MULTIPLE INPUTS
+    //   function test_numword_ones()
+    // {
+    //     $test_NumWord_Ones = new Numword;
+    //     $input_array = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    //     $expected_results = ['','One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    //     $result_array = array();
+    //
+    //     foreach ($input_array as $input)
+    //     {
+    //       array_push($result_array, $test_NumWord_Ones->process_thru_nineteen($input));
+    //     }
+    //     $this->assertEquals($expected_results, $result_array);
+    // }
+
+ ?>
